323ce7d20d4581bc1e93136d9eff600c27c010c8
Denis Riabtsev <68712122+1KitCat1@users.noreply.github.com>
2023-09-07 13:14:59 +0300

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use winterfell::math::{fields::f128::BaseElement, FieldElement};
+use winterfell::{TraceTable, Serializable};
+
+const INV_ALPHA: u128 = 226854911280625642308916371969163307691;
+const CONST_42: BaseElement = BaseElement::new(42);
+
+
+fn main() {
+
+}
+
